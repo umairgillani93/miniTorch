b6b0e28bc4adc68559ae226dcd9a4800dc0feb03
umairgillani93 <umairgillani93@gmail.com>
2026-03-18 01:28:52 +0500

    Training loop forward pass completed

--- main.c
+++ main.c
@@ -14,11 +14,17 @@ int main() {
 	srand(time(NULL));
 	int shape[2] = {SEQ_LEN, EMB_DIM};
 	
+	// Creating actual data tensor
 	Tensor *T = tensor_create(2, shape);
 	int size = tensor_size(T);
-	//int num_batches = SEQ_LEN / BATCH_SIZE;
 
+	// Target tensor to compare the output against
+	int shape_target[2] = {SEQ_LEN, EMB_DIM};
+	Tensor *target = tensor_create(ndim, shape_target);
+
+	// define batches for Actual tensor
 	int num_chunks = SEQ_LEN / BATCH_SIZE;
+
 	for (int b = 0; b < num_chunks; b++) {
 
     float *batch_ptr = T->data + b * BATCH_SIZE * EMB_DIM;
@@ -27,17 +33,37 @@ int main() {
     Tensor *batch_tensor = tensor_create(2, shape_local);
 		memcpy(batch_tensor->data, batch_ptr, BATCH_SIZE * EMB_DIM * sizeof(float));
 
+		// Create MHA for multihead attention scores and run mha_forward pass
 		MHA *m = mha_create(HEADS, BATCH_SIZE, EMB_DIM);
-		//tensor_shape(batch_tensor);
-		//tensor_shape(m->wq);
-		//tensor_shape(m->Q);
-		//printf("heads: %d\n", m->num_heads);
-		//printf("dk: %d\n", m->dk);
-		//break;
-		Tensor *multi_head = mha_forward(batch_tensor, m);
-		memcpy(batch_ptr, batch_tensor->data, BATCH_SIZE * EMB_DIM * sizeof(float));
-		tensor_shape(multi_head);
-}
+		Tensor *attn_score = mha_forward(batch_tensor, m);
+
+		// Apply layer_norm
+		Tensor *ln1 = layer_norm(attn_score);
+
+		// Create FFN feed-forward NN and run ffn_forward pass
+		FFN *f = ffn_create(EMB_DIM, 128);
+		Tensor *ffn_ln = ffn_forward(ln1, f);
+		
+		// Apply layer_norm
+		Tensor *ln2 = layer_norm(ffn_ln);
+		
+		// Copy the chunks back to main tensor
+		memcpy(batch_ptr, ln2->data, BATCH_SIZE * EMB_DIM * sizeof(float));
+
+		// free memory
+		//tensor_free(batch_tensor);
+		//tensor_free(attn_score);
+		//tensor_free(ln1);
+		//tensor_free(ffn_ln);
+		//tensor_free(ln2);
+	}
+
+	Tensor *loss = tensor_mse_loss(T, target);
+	tensor_get(target);
+	//tensor_get(loss);
+	//tensor_get(loss);
+	//tensor_get(loss);
+	//tensor_shape(loss);
 
 	return 0;
 }
