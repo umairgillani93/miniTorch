3318d7400105abf08f1307bc78068f20da86cb71
umairgillani93 <umairgillani93@gmail.com>
2026-04-14 17:36:13 +0500

    arena_init() and arean_alloc() test

---
+++ test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct {
+	char *base;
+	int size;
+	int offset;
+} Arena;
+
+void arena_init(Arena *A, int size) {
+	A->base = (char *)malloc(size);
+	A->size = size;
+	A->offset = 0;
+}
+
+void* arena_alloc(Arena *A, int size) {
+	void *ptr = A->base + A->offset;
+	A->offset += size * sizeof(int);
+	return ptr;
+}	
+
+
+int main() {
+	int SIZE = 1024 * 1024;
+
+	Arena *A = malloc(sizeof(Arena));;
+	arena_init(A, SIZE);
+	printf("Arean created!\n");
+
+	int arr_size = 4;
+	int *arr = (int *)arena_alloc(A, arr_size);
+
+	for (int i = 0; i < arr_size; i++) {
+		printf("%d\n", arr[i]);
+	}
+
+
+	return 0;
+
+}
