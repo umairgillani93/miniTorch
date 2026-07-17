fed43b56f00b505760c4b18acdc1cf36d2e2ab7a
umairgillani93 <umairgillani93@gmail.com>
2026-07-17 12:47:29 +0500

    fixed bug with tokenizer encoder()

--- tokenizer/tok.py
+++ tokenizer/tok.py
@@ -13,7 +13,7 @@ class Tokenizer:
         token ids.
         '''
         text = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-        text = [x.strip() for x in text.split() if x.strip()]
+        text = [x.strip() for x in text if x.strip()]
         text = [x if x in self.str_to_int else "<|unk|>"
                 for x in text]
         
@@ -31,4 +31,20 @@ class Tokenizer:
         text = re.sub(r'\s+([,.:;?_!"()\'])', r'\1', text)
         return text
 
+
+
+if __name__ == "__main__":
+    text = """
+        “wait!” she screamed. “don’t go!” but he was already halfway out the door.
+        She ran, faster than she ever thought possible, but stopped. Why?
+        He wasn’t running from her. He was running to her.
+        “Are you—?” she gasped. “I thought you—”
+        He smiled, quietly, holding out a box. “It’s for you.”
+        She froze. “What is it?”
+        A ring. No words needed.
+        Her breath caught, and then—“Yes.”
+        They kissed, finally, with the whole world quiet around them.
+        And they lived happily ever after.
+    """
+    t = Tokenizer(vocab)
 
