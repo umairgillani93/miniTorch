522c0499392fe1fa5c800eda7c665d857815174c
umairgillani93 <umairgillani93@gmail.com>
2026-07-21 11:24:15 +0500

    optimizations done for benchmarks (branch commit benchmarks)

--- benchmarks/mm.c
+++ benchmarks/mm.c
@@ -10,6 +10,19 @@ Tensor *compare_matmul(Arena *A, Tensor *x, Tensor *y) {
 	Tensor *z = tensor_matmul(A, x, y);
 	return z;
 }
+/* 
+	for (int r = 0; r < a_rows; r++) {
+		for (int c = 0; c < b_cols; c++) {
+			float sum = 0.0f;
+			for (int k = 0; k < a_cols; k++) {
+				sum += (a->data[(r * a_cols + k)] *
+					 	b->data[(k * b_cols + c)]);
+			}
+			out->data[r * b_cols + c] = sum;
+		}
+	}
+ 
+*/
 
 
 
@@ -36,8 +49,24 @@ int main() {
 	Tensor *y = tensor_create_new(A,ndim,  shape_y);
 	tensor_randomize_weights(y);
 
+	int x_rows = x->shape[0];
+	int x_cols = x->shape[1];
+
+	int y_rows = y->shape[0];
+	int y_cols = y->shape[1];
+
+	float total = 0.0f;
 	clock_t start_time = clock();
-	Tensor *z = compare_matmul(A, x, y);
+	for (int r = 0; r < x_rows; r++) {
+		for (int c = 0; c < y_cols; c++) {
+			float sum = 0.0f;
+			for (int k = 0; k < x_cols; k++) {
+				sum += (x->data[(r * x_cols + k)] * 
+						y->data[(k * y_cols + c)]);
+			}
+			total += sum;
+		}
+	}
 	clock_t end_time = clock();
 	double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
 	printf("total time in ms: %f\n", time_taken * 1000);
