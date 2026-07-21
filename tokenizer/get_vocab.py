05ec01775d66fe593a0d2e3d206cff8f2e0580e2
umairgillani93 <umairgillani93@gmail.com>
2026-07-21 09:41:18 +0500

    tokenizer final

--- tokenizer/get_vocab.py
+++ tokenizer/get_vocab.py
@@ -5,6 +5,8 @@ from nltk.corpus import words
 # Load the complete list
 english_vocab = " ".join(words.words())
 
+# get english vocab from nltk and dump in a 
+# text file for developing purpose
 with open('./input.txt', 'w', encoding = 'utf-8') as f:
     f.write(english_vocab)
     print('done')
