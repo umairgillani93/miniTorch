c5c5feb0901de9079e50da6e0755580c74d389f6
umairgillani93 <umairgillani93@gmail.com>
2026-07-21 10:02:40 +0500

    post zeros padding added for sequences

--- tokenizer/utils.py
+++ tokenizer/utils.py
@@ -17,9 +17,29 @@ class Utility:
 
         return all_words
 
+    def pad_sequence(self, sequences):
+        '''post pad the list of sequences to zeros'''
+        S = []
+        max_len = 0
+        for s in sequences:
+            max_len = max(max_len, len(s.split()))
+
+        for s in sequences:
+            words = s.split()
+            if (len(words) < max_len):
+                N = max_len - len(words)
+                words.extend(["0"] * N)
+                S.append(words)
+            else:
+                S.append(words)
+
+        return S
 
 
-
-
-
-
+if __name__ == '__main__':
+    u = Utility()
+    S = ['this is a first len',
+        'this is second',
+        'abc'
+        ]
+    print(u.pad_sequence(S))
